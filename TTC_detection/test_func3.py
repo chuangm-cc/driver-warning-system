dfa73313632f4ef8fb25b8652165afae915adb7d
chuangm-cc <chuangm@andrew.cmu.edu>
2025-12-22 00:51:48 -0500

    Update test_func3.py

--- TTC_detection/test_func3.py
+++ TTC_detection/test_func3.py
@@ -37,12 +37,11 @@ def formula_func(kp1,kp2,matches,frame,box,show_img):
         for j in range(i,kp_range):
             total_dis_1+=get_dis(kp1[matches[i].queryIdx].pt , kp1[matches[j].queryIdx].pt)
             total_dis_2+=get_dis(kp2[matches[i].trainIdx].pt , kp2[matches[j].trainIdx].pt)
-            #count+=1
-        #print(matches[i].distance)
+
 
     len_1=total_dis_1
     len_2=total_dis_2
-    #print(len_1,len_2)
+
 
     if len_2==0:
         cv2.putText(frame,box[-1]+'No matching found', (int(box[0]), int(box[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
@@ -61,15 +60,13 @@ def formula_func(kp1,kp2,matches,frame,box,show_img):
     for i in range(0,loop_num):
         cv2.circle(show_img,(int(kp1[matches[i].queryIdx].pt[0]),int(kp1[matches[i].queryIdx].pt[1])),2,(0,0,255),-1)
     show_img=cv2.resize(show_img, (0,0) ,fx=4,fy=4)
-    #cv2.imshow('img',show_img)
 
-    #cv2.waitKey(0)
 
 
     filename = 'static_fea_dis.txt'
     with open (filename,'a') as file_object:
         if TTC>-50 and TTC<50:
-        #if abs(s-1)<0.1:
+
             file_object.write(str(TTC)+'\n') 
     
     if s==1:
@@ -128,7 +125,7 @@ def get_result(frame,pre_frame,box,pre_box,pre_cal_time):
 
     pre_img_box=pre_frame[int(pre_box[1]):int(pre_box[3]),int(pre_box[0]):int(pre_box[2])]
     
-    #cv2.imshow('box', pre_img_box)
+
     img_box=frame[int(box[1]):int(box[3]),int(box[0]):int(box[2])]
     show_img=img_box.copy()
 
@@ -142,18 +139,14 @@ def get_result(frame,pre_frame,box,pre_box,pre_cal_time):
     edge2=cv2.Canny(pre_img_box,threshold1=150,threshold2=200,L2gradient=True)
     ''' using sift'''
     sift = cv2.SIFT_create()
-    #orb = cv2.ORB_create()
-    # kp1 = sift.detect(img_box,None)
-    # kp2 = sift.detect(pre_img_box,None)
+
 
     ''' using harris'''
     kp1 = detect_corners_harris(edge1)
     kp2 = detect_corners_harris(edge2)
-    #img2 = cv2.drawKeypoints(edge1, keypoints=kp1, outImage=None, color=(255,0,0))
-    #cv2.imshow('cam1_time0', img2)
-    #print(len(kp1))
+
     kp1,des1 = sift.compute(img_box,kp1)
-    #kp2 = sift.detect(pre_img_box,None)
+
     kp2,des2 = sift.compute(pre_img_box,kp2)
     
     if des1 is None or des2 is None:
@@ -174,24 +167,20 @@ def get_result_2(frame,pre_frame,box,pre_box,pre_cal_time):
 
     pre_img_box=pre_frame[int(pre_box[1]):int(pre_box[3]),int(pre_box[0]):int(pre_box[2])]
     
-    #cv2.imshow('box', pre_img_box)
+
     img_box=frame[int(box[1]):int(box[3]),int(box[0]):int(box[2])]
     show_img=img_box.copy()
 
     img_box = cv2.cvtColor(img_box, cv2.COLOR_BGR2GRAY)
     pre_img_box = cv2.cvtColor(pre_img_box, cv2.COLOR_BGR2GRAY)
 
-    #img_box=cv2.Canny(img_box,threshold1=100,threshold2=200,L2gradient=True)
-    #pre_img_box=cv2.Canny(pre_img_box,threshold1=100,threshold2=200,L2gradient=True)
-    #cv2.imshow('canny',img_box)
-    #cv2.waitKey(0)
+
 
 
     sift = cv2.SIFT_create()
     kp1 = sift.detect(img_box,None)
     kp2 = sift.detect(pre_img_box,None)
     kp1,des1 = sift.compute(img_box,kp1)
-    #kp2 = sift.detect(pre_img_box,None)
     kp2,des2 = sift.compute(pre_img_box,kp2)
     
     if des1 is None or des2 is None:
@@ -216,10 +205,6 @@ def get_result_3(frame,pre_frame,box,pre_box,pre_cal_time):
     const_wit=int(wit/4)
     const_hei=int(hei/4)
 
-    #pre_img_box=pre_frame[int(pre_box[1]):int(pre_box[3]),int(pre_box[0]):int(pre_box[2])]
-    #pre_img_box=cv2.resize(pre_img_box, (int(box[2])-int(box[0]),int(box[3])-int(box[1])))
-    
-    #cv2.imshow('box', pre_img_box)
     pre_img_box=pre_frame[int(box[1]):int(box[3]),int(box[0]):int(box[2])]
     show_pre_img=pre_img_box.copy()
 
@@ -230,46 +215,30 @@ def get_result_3(frame,pre_frame,box,pre_box,pre_cal_time):
     img_box = cv2.cvtColor(img_box, cv2.COLOR_BGR2GRAY)
     pre_img_box = cv2.cvtColor(pre_img_box, cv2.COLOR_BGR2GRAY)
 
-    #img_box=cv2.Canny(img_box,threshold1=100,threshold2=200,L2gradient=True)
-    #pre_img_box=cv2.Canny(pre_img_box,threshold1=100,threshold2=200,L2gradient=True)
-    #cv2.imshow('canny',img_box)
-    #cv2.waitKey(0)
 
 
     p0=cv2.goodFeaturesToTrack(pre_img_box,mask=None, **feature_params)
     if p0 is None:
         return
-    #img_shape=pre_img_box.shape()
-    #img_box=cv2.resize(img_box, (int(pre_box[3]-pre_box[1]),int(pre_box[2]-pre_box[0])))
+
 
     p1,st,err=cv2.calcOpticalFlowPyrLK(pre_img_box,img_box,p0,None,**lk_params)
-    #print(p1)
+
     if p1 is not None:
         good_new = p1[st==1]
         good_old = p0[st==1]
     if len(good_old)==0 or len(good_new)==0:
         return
 
-    #print(good_new[0][0])
     loop_num=len(good_old)
     for i in range(0,loop_num):
         cv2.circle(show_pre_img,(int(good_old[i][0]),int(good_old[i][1])),2,(0,0,255),-1)
     show_pre_img=cv2.resize(show_pre_img, (0,0) ,fx=4,fy=4)
-    #cv2.imshow('img',show_pre_img)
-    
-    #img2 = cv2.drawKeypoints(pre_img_box, keypoints=good_old, outImage=None, color=(255,0,0))
-    #cv.imshow('cam1_time0', img2)
-    #print(good_old[0],good_new[0])
-    
-    #kp = cv.KeyPoint(x, y)
 
     st_0=good_old[0]
     st_1=good_new[0]
     total_dis_0=0
     total_dis_1=0
-    #count=0
-    #print(len(good_new),len(good_old))
-    #kp_range=min(5,len(good_new))
     kp_range=len(good_new)
     for i in range(0,kp_range-1):
         for j in range(0,kp_range):
@@ -279,26 +248,21 @@ def get_result_3(frame,pre_frame,box,pre_box,pre_cal_time):
     if total_dis_0==0:
         return
     len_0=total_dis_0
-    #/count
+
     len_1=total_dis_1
-    #/count
+
     s=len_1/len_0
-    #print(s)
-    #print(len_0,len_1)
     
     if s==1:
         return
     delta_t=delta_time
     #print(delta_t)
     TTC=round(delta_t/(s-1),3)
-    #print('TTC: ',TTC)
     print('match',len(good_new))
 
     #cv2.waitKey(0)
     filename = 'static_fea_dis.txt'
     with open (filename,'a') as file_object:
-        #if TTC>0 and TTC<50:
-        #if abs(s-1)<0.1:
         if TTC>0 and TTC <50:
             file_object.write(str(TTC)+'\n') 
 
@@ -354,8 +318,6 @@ def detection(img, boxs):
 
 def catch_video(name='my_video', video_index=2):
     global delta_time
-    # cv2.namedWindow(name)
-    #cap = cv2.VideoCapture(video_index) # creat camera class
     cap = cv2.VideoCapture(test_video) # creat camera class
     #cap = cv2.VideoCapture('./result.mp4') # creat camera class
 
@@ -408,27 +370,17 @@ def catch_video(name='my_video', video_index=2):
             function(frame,boxs,pre_boxs,pre_frame,pre_cal_time)
             pre_cal_time=time.time()
 
-            #print('time of max loss:'+str(time.time()-start_func))
             loss_time=time.time()-start_func
 
-            
 
-            #cv2.imshow(name, frame) # show result
-            #videoWriter_1.write(frame)
-            
-            #print(str(time.time()-start))
 
             pre_catch=catch
             pre_frame = frame 
-            #pre_results = model(pre_frame)
             pre_boxs = boxs
-            #print('time for whole process:'+str(time.time()-start))
-            #print('ratio of max loss caused by different time:'+str(round(loss_time/(time.time()-start),10)))
+        
         else:
             start=time.time()  
             catch, frame = cap.read()  # read every frame of camera
-            # cv2.imshow(name, frame) # show result
-            #print(str(time.time()-start))
         
         
         key = cv2.waitKey(10)
@@ -442,10 +394,7 @@ def catch_video(name='my_video', video_index=2):
         '''
         print("FPS:", 1/(time.time()-start))
 
-    # release camera
-    
-    #cap.release()
-    #cv2.destroyAllWindows()
+
 
 def catch_video_2():
     global delta_time
